156e21694f9a2fdbd028a07644323f02ae024e7f
Joseph Rushton Wakeling <joe@webdrake.net>
2013-11-27 18:23:49 +0100

    MersenneTwisterEngine should be final class.

--- std/random2/generator.d
+++ std/random2/generator.d
@@ -201,11 +201,11 @@ unittest
     static assert(isSeedable!(seedRng));
 }
 
-class MersenneTwisterEngine(UIntType,
-                            size_t w, size_t n, size_t m, size_t r,
-                            UIntType a, size_t u, UIntType d, size_t s,
-                            UIntType b, size_t t,
-                            UIntType c, size_t l, UIntType f)
+final class MersenneTwisterEngine(UIntType,
+                                  size_t w, size_t n, size_t m, size_t r,
+                                  UIntType a, size_t u, UIntType d, size_t s,
+                                  UIntType b, size_t t,
+                                  UIntType c, size_t l, UIntType f)
 if (isUnsigned!UIntType)
 {
   private:
